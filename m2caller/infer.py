a573407d989e8205d20deacc2709745113e5e93a
HongYeNB <165907590+HongYeNB@users.noreply.github.com>
2025-09-17 23:29:02 +0800

    Update infer.py

--- m2caller/infer.py
+++ m2caller/infer.py
@@ -3,6 +3,7 @@ from torch.utils.data import DataLoader
 from m2caller.utils.dataset import NumpyRecordsDataset, collate_variable_windows
 from m2caller.utils.common import get_device
 from m2caller.models.m2caller import M2CallerModel
+from m2caller.models.m2caller_mamba import M2CallerMambaModel
 from m2caller.decoding.greedy import greedy_decode
 from m2caller.utils.constants import TOKENS, BLANK
 from tqdm import tqdm
@@ -14,6 +15,7 @@ def main():
     ap.add_argument('--out_fasta', required=True)
     ap.add_argument('--no_cuda', action='store_true')
     ap.add_argument('--half', action='store_true')
+    ap.add_argument('--arch', type=str, default='mamba2', choices=['mamba2','ssm-lite'])
     args = ap.parse_args()
 
     device = get_device(args.no_cuda)
@@ -22,7 +24,16 @@ def main():
 
     ckpt = torch.load(args.ckpt, map_location=device)
     margs = ckpt.get('args', {})
-    model = M2CallerModel(**{k:v for k,v in margs.items() if k in ['d_model','layers','heads','ssm_dim']}, vocab_size=len(TOKENS))
+    if args.arch == 'mamba2':
+        model = M2CallerMambaModel(d_model=margs.get('d_model',512),
+                                   layers=margs.get('layers',4),
+                                   ssm_d_state=margs.get('ssm_d_state',16),
+                                   ssm_d_conv=margs.get('ssm_d_conv',4),
+                                   vocab_size=len(TOKENS))
+    else:
+        model = M2CallerModel(**{k:v for k,v in margs.items()
+                                 if k in ['d_model','layers','heads','ssm_dim']},
+                              vocab_size=len(TOKENS))
     model.load_state_dict(ckpt['model'])
     model.to(device).eval()
     if args.half:
@@ -33,8 +44,6 @@ def main():
             batch = batch.to(device)
             log_probs, out_lengths = model(batch, lengths.to(device))
             seqs_idx = greedy_decode(log_probs, out_lengths, blank=BLANK)
-            # write one concatenated sequence per original read id
-            # since we split windows, we join their decoded strings
             concat = {}
             for rid, seq in zip(read_ids, seqs_idx):
                 s = ''.join(TOKENS[i] for i in seq if i != BLANK)
